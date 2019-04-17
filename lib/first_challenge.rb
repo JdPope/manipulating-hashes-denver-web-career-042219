92af01051183cac9319826e37d060f6a1e926dac
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 17:07:03 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         flavor.delete_if("strawberry")
-
+end
 
   #remember to return your newly altered contacts hash!
  return contacts
